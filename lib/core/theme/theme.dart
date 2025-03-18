156305aabed39c5eac9081713d751a2adf80e752
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-03-19 03:38:12 +0530

    Modifying auth

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,27 @@
+import 'package:gaugyam/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+    borderSide: BorderSide(color: color, width: 3),
+    borderRadius: BorderRadius.circular(10),
+  );
+  static final lightThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.whiteColor,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppPallete.whiteColor,
+      elevation: 0,
+    ),
+    chipTheme: ChipThemeData(
+      color: MaterialStateProperty.all(AppPallete.primaryFgColor),
+      side: BorderSide.none,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(27),
+      border: _border(),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+      errorBorder: _border(AppPallete.errorColor),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gaugyam/core/theme/theme.dart';
+import 'package:gaugyam/features/auth/pages/auth_screen.dart';
 import 'package:gaugyam/features/auth/pages/phoneauth_page.dart';
+import 'package:gaugyam/features/home/home_screen.dart';
 import 'package:gaugyam/firebase_options.dart';
 
 void main() async{
@@ -22,12 +26,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: AppTheme.lightThemeMode,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: PhoneAuthScreen(),
+      home: AuthWrapper(),
     );
   }
 }
+// Add this to your imports
 
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // If the user is logged in
+        if (snapshot.hasData) {
+          // Return your home screen or dashboard
+          return PhoneAuthScreen(); // Replace with your actual home page
+        }
+        // If the user is not logged in
+        return PhoneAuthScreen();
+      },
+    );
+  }
+}
