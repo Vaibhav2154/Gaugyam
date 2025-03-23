83d3d69353109a83af370b41feaa9108de916a0d
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-03-23 16:39:19 +0530

    Appointiments scheduling

---
+++ lib/features/appointments/widgets/auth_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:gaugyam/core/theme/app_pallete.dart';
+
+class AppointmentField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  final TextInputType keyboardType;
+  const AppointmentField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+    required String? Function(dynamic value) validator, required this.keyboardType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(color: AppPallete.greyColor),
+        filled: true,
+        contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppPallete.gradient1, width: 2),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: AppPallete.transparentColor,
+            width: 1.5,
+          ),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter $hintText';
+        }
+        return null;
+      },
+      obscureText: isObscureText,
+    );
+  }
+}
